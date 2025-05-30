8aa48940eb30e9fe8a65c9a66b60dcd3d1b0a461
Polina Borysenko <polina.borysenko@nure.ua>
2025-05-30 19:03:55 +0200

    Added enum extensions to display enum values as strings.

---
+++ CarDealership/Extensions/EnumExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace CarDealership.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            var memberInfo = enumValue.GetType().GetMember(enumValue.ToString());
+            var displayAttr = memberInfo[0].GetCustomAttribute<DisplayAttribute>();
+            return displayAttr?.Name ?? enumValue.ToString();
+        }
+    }
+}
