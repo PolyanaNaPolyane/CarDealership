81579b73c0b54717715606a9ab7b088d12a44f72
Polina Borysenko <polina.borysenko@nure.ua>
2025-05-30 19:21:13 +0200

    Added forms extension to open child forms.

---
+++ CarDealership/Extensions/FormExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace CarDealership.Extensions
+{
+    public static class FormExtensions
+    {
+        public static void Show<T>(this Form form) where T : Form, new()
+        {
+            var formToShow = new T();
+            formToShow.FormClosed += (s, e) => form.Show();
+            formToShow.Show();
+            form.Hide();
+        }
+    }
+}
